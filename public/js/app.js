7077820e8b46744b6ba7388337a02b6a7b506a96
Emmanuel Ndukwe <emrain02@gmail.com>
2018-01-08 17:51:44 +0100

    feature(landing page): add landing page with effective ui/ux [Finishes #153742464]

--- public/js/app.js
+++ public/js/app.js
@@ -1,52 +1,80 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives'])
-  .config(['$routeProvider',
-      function($routeProvider) {
-          $routeProvider.
-          when('/', {
-            templateUrl: 'views/index.html'
-          }).
-          when('/app', {
-            templateUrl: '/views/app.html',
-          }).
-          when('/privacy', {
-            templateUrl: '/views/privacy.html',
-          }).
-          when('/bottom', {
-            templateUrl: '/views/bottom.html'
-          }).
-          when('/signin', {
-            templateUrl: '/views/signin.html'
-          }).
-          when('/signup', {
-            templateUrl: '/views/signup.html'
-          }).
-          when('/choose-avatar', {
-            templateUrl: '/views/choose-avatar.html'
-          }).
-          otherwise({
-            redirectTo: '/'
-          });
-      }
-  ]).config(['$locationProvider',
-    function($locationProvider) {
-        $locationProvider.hashPrefix("!");
+angular
+  .module('mean', [
+    'ngCookies',
+    'ngResource',
+    'ui.bootstrap',
+    'ui.route',
+    'mean.system',
+    'mean.directives'
+  ])
+  .config([
+    '$routeProvider',
+    function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/index.html'
+        })
+        .when('/app', {
+          templateUrl: '/views/app.html'
+        })
+        .when('/privacy', {
+          templateUrl: '/views/privacy.html'
+        })
+        .when('/bottom', {
+          templateUrl: '/views/bottom.html'
+        })
+        .when('/signin', {
+          templateUrl: '/views/signin.html'
+        })
+        .when('/signup', {
+          templateUrl: '/views/signup.html'
+        })
+        .when('/choose-avatar', {
+          templateUrl: '/views/choose-avatar.html'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
     }
-  ]).run(['$rootScope', function($rootScope) {
-  $rootScope.safeApply = function(fn) {
-    var phase = this.$root.$$phase;
-    if(phase == '$apply' || phase == '$digest') {
-        if(fn && (typeof(fn) === 'function')) {
+  ])
+  .config([
+    '$locationProvider',
+    function ($locationProvider) {
+      $locationProvider.hashPrefix('!');
+    }
+  ])
+  .run([
+    '$rootScope',
+    function ($rootScope) {
+      $rootScope.safeApply = function (fn) {
+        const phase = this.$root.$$phase;
+        if (phase == '$apply' || phase == '$digest') {
+          if (fn && typeof fn === 'function') {
             fn();
+          }
+        } else {
+          this.$apply(fn);
         }
-    } else {
-        this.$apply(fn);
-      }
+      };
+    }
+  ])
+  .run([
+    'DonationService',
+    function (DonationService) {
+      window.userDonationCb = function (donationObject) {
+        DonationService.userDonated(donationObject);
+      };
+    }
+  ])
+  .controller('ScrollAfix', ($scope, $location, $anchorScroll) => {
+    $scope.scrollTo = function (id) {
+      $location.hash(id);
+      $anchorScroll();
+      setTimeout(function(){
+        window.scrollTo(window.pageXOffset, window.pageYOffset - 55);
+        },100);
     };
-  }]).run(['DonationService', function (DonationService) {
-    window.userDonationCb = function (donationObject) {
-      DonationService.userDonated(donationObject);
-    };
-  }]);
+  });
 
 angular.module('mean.system', []);
 angular.module('mean.directives', []);
