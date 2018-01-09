fdeacc7b88ffeaa2a769a54a1591a4061f040abf
Justin Nebo <nebojustin@gmail.com>
2018-01-09 17:05:38 +0100

    feature(create game): add game model, controller and route
    [Starts 153742465]

---
+++ app/models/game.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const GameSchema = new Schema({
+  userId: [],
+  gameId: {
+    type: String,
+  },
+  players: [],
+  round: {
+    type: Number,
+  },
+  winner: {
+    type: String,
+    default: '',
+  }
+}, { timestamps: true });
+
+export default mongoose.model('GameSession', GameSchema);
