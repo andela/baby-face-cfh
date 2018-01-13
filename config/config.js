080b72606511107c365d10033c64618901c4860f
Emmanuel Ndukwe <emasysnd@gmail.com>
2018-01-13 16:42:57 +0100

    fix linting errors

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ const _ = require('underscore');
 // Load app configuration
 module.exports = _.extend(
   require(`${__dirname}/../config/env/all.js`),
+  /* eslint-disable */
   require(`${__dirname}/../config/env/${process.env.NODE_ENV}.json`) || {},
 );
