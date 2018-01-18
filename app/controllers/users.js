ebf5f1a4522ad4b369308f3be0b218361b1e20b4
sccofield <michael_umanah@yahoo.com>
2018-01-18 08:45:03 +0100

    feature(friends-connect):add notification model [#153742474]

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -335,3 +335,68 @@ exports.sendInvite = (req, res) => {
   });
 };
 
+// Add Friends
+
+exports.addFriend = (req, res) => {
+  const { friendId, friendName, friendEmail } = req.body;
+  const friendData = { friendId, friendName, friendEmail };
+  const userId = req.decoded.id;
+  User.findOneAndUpdate(
+    {
+      _id: userId
+    },
+    {
+      $push: { friends: friendData }
+    }
+  ).then(() => {
+    res.status(200).json({
+      message: 'Friend Added Succesfully'
+    });
+  })
+    .catch((error) => {
+      res.status(500).json({
+        error,
+        message: 'Internal Server Error'
+      });
+    });
+};
+
+exports.getFirendsList = (req, res) => {
+  const userId = req.decoded.id;
+
+  User.find({
+    _id: userId
+  }).then((user) => {
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found'
+      });
+    }
+    return res.status(200).json(user[0].friends);
+  })
+    .catch((error) => {
+      res.status(500).json({
+        error,
+        message: 'Internal Server Error'
+      });
+    });
+};
+
+exports.deleteFriend = (req, res) => {
+  const userId = req.decoded.id;
+  const { friendId } = req.params;
+  User.findOneAndUpdate(
+    {
+      _id: userId
+    },
+    {
+      $pull: { friends: { friendId } }
+    },
+    { multi: true }
+  ).then(() => {
+    res.status(200).json({
+      message: 'Friend removed sucessfully!'
+    });
+  });
+};
+
